a87c9c94540c63f68cca105498d9740767057826
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2023-10-04 15:23:46 -0400

    init to set up a netlify function

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  useEffect(()=>{
+    axios.get('https://pokeapi.co/api/v2/pokemon-form/charizard')
+    .then(response => {
+        // Handle the response from your backend
+        console.log(response.data)
+      })
+      .catch(error => {
+        // Handle errors
+      });
+
+  },[])
+
+  return (
+    <>
+    <h1>Hello</h1>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
