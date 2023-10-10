e618c1e5e09482c9b1eafa9c3e03563f696b6138
Zoe Daniels <7177924+zkdan@users.noreply.github.com>
2023-10-10 15:11:43 -0400

    narrowing to LT in UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import {useState } from 'react'
-import './App.css'
+import {useState} from 'react';
+import './App.css';
 import axios from 'axios';
 import Loader from './Loader';
 function App() {
   const [response, setResponse] = useState<string>('')
   const [loading, setLoading] = useState<boolean>(false);
   const [question, setQuestion] = useState<string>('');
-  const [personality, setPersonality] = useState<string>('');
   const handleClick = ()=>{
     setLoading(true)
-    axios('https://quiet-douhua-b0fd6b.netlify.app/.netlify/functions/openai-function', {params:{question, personality}}).then(res =>{
+    axios('https://quiet-douhua-b0fd6b.netlify.app/.netlify/functions/openai-function', {params:{question}}).then(res =>{
       console.log(res);
       setResponse(res.data);
       setLoading(false);
@@ -23,20 +22,11 @@ function App() {
     e.preventDefault();
     setQuestion('');
   }
-  const handleChangePersona: React.ChangeEventHandler<HTMLSelectElement>=(e)=>{
-    setPersonality(e.target.value)
-  }
   return (
     <>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Statue_of_Lao_Tzu_in_Quanzhou.jpg/1599px-Statue_of_Lao_Tzu_in_Quanzhou.jpg?20090820205352" alt="Statue of Lao Tzu in Quanzhou" />
     <form onSubmit={handleSubmit}>
-      <select name="" id="" onChange={handleChangePersona}>
-        <option value="American linguist Dave Kush">Dave</option>
-        <option value="Chinese haiku poet Lao tzu">laotsu</option>
-        <option value="Rapper Ice T">T</option>
-        <option value="French novelist Anaïs Nin">nin</option>
-        <option value="French chef Remy the rat">remy</option>
-      </select>
-    <label htmlFor="question">Ask any question</label>
+      <label htmlFor="question">Ask any question</label>
       <input id="question" type="text" onChange={handleChange} value={question}/>
       <button onClick={handleClick}>Hit it, baby</button>
     </form>
